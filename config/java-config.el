8de7430fd93d2b1f887ed8b76f5ff7d2ae997bd4
Ryan Smith <rnsmith2@gmail.com>
2014-06-18 10:45:09 -0700

    Bind eclim find/pop definitions

--- config/java-config.el
+++ config/java-config.el
@@ -6,3 +6,35 @@
 
 (require 'dtrt-indent)
 (add-hook 'java-mode-hook 'dtrt-indent-mode)
+
+(defvar eclim-tags-location-ring (make-ring 20))
+
+(defun eclim-goto-definition (point)
+  "Goto definition using semantic-ia-fast-jump
+save the pointer marker if tag is found"
+  (interactive "d")
+  (condition-case err
+      (progn
+        (ring-insert eclim-tags-location-ring (point-marker))
+        (eclim-java-find-declaration))
+    (error
+     ;;if not found remove the tag saved in the ring
+     (set-marker (ring-remove eclim-tags-location-ring 0) nil nil)
+     (signal (car err) (cdr err)))))
+
+(defun eclim-pop-tag-mark ()
+  "popup the tag save by eclim-goto-definition"
+  (interactive)
+  (if (ring-empty-p eclim-tags-location-ring)
+      (message "%s" "No more tags available")
+    (let* ((marker (ring-remove eclim-tags-location-ring 0))
+           (buff (marker-buffer marker))
+           (pos (marker-position marker)))
+      (if (not buff)
+          (message "Buffer has been deleted")
+        (switch-to-buffer buff)
+        (goto-char pos))
+      (set-marker marker nil nil))))
+
+(define-key java-mode-map (kbd "M-.") 'eclim-goto-definition)
+(define-key java-mode-map (kbd "M-,") 'eclim-pop-tag-mark)
