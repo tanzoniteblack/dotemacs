8b73203d64b07d96d749e419a694c1e585e643c3
Ryan Smith <rnsmith2@gmail.com>
2014-08-08 07:49:58 -0700

    Whitespace fix

--- config/smartparens-personal-config.el
+++ config/smartparens-personal-config.el
@@ -12,9 +12,9 @@
 
 ;; custom keybindings for smartparens mode
 (define-key smartparens-mode-map (kbd "C-<left>") 'sp-forward-barf-sexp)
-(define-key smartparens-mode-map (kbd "M-(")'sp-forward-barf-sexp)
+(define-key smartparens-mode-map (kbd "M-(") 'sp-forward-barf-sexp)
 (define-key smartparens-mode-map (kbd "C-<right>") 'sp-forward-slurp-sexp)
-(define-key smartparens-mode-map (kbd "M-)")'sp-forward-slurp-sexp)
+(define-key smartparens-mode-map (kbd "M-)") 'sp-forward-slurp-sexp)
 
 (define-key smartparens-strict-mode-map (kbd "M-d") 'sp-kill-sexp)
 
