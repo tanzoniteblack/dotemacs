1813df63ea300a1e88f60ee10a38b73c6fc47a31
Ryan Smith <rnsmith2@gmail.com>
2014-08-21 08:56:22 -0700

    Try diminish as hook

--- config/diminish-config.el
+++ config/diminish-config.el
@@ -3,8 +3,7 @@
 (diminish 'paredit-mode "Par")
 (diminish 'elisp-slime-nav-mode "")
 (diminish 'undo-tree-mode "")
-(eval-after-load 'git-gutter-mode
-  (diminish 'git-gutter-mode ""))
+(add-hook 'git-gutter-mode-hook (lambda () (diminish 'git-gutter-mode "")))
 (diminish 'highlight-symbol-mode "")
 (diminish 'eldoc-mode "")
 (diminish 'magit-auto-revert-mode "")
