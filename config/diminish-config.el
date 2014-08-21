79d22fc2b15efb0059cd8a6203cbd3889f6a1ecc
Ryan Smith <rnsmith2@gmail.com>
2014-08-21 08:47:23 -0700

    Undo quotes

--- config/diminish-config.el
+++ config/diminish-config.el
@@ -1,15 +1,15 @@
 (require 'diminish)
 
-(defvar diminish-modes '(('paredit-mode "Par")
-						 ('elisp-slime-nav-mode "")
-						 ('undo-tree-mode "")
-						 ('git-gutter-mode "")
-						 ('highlight-symbol-mode "")
-						 ('eldoc-mode "")
-						 ('magit-auto-revert-mode "")))
+(defvar diminish-modes '((paredit-mode "Par")
+						 (elisp-slime-nav-mode "")
+						 (undo-tree-mode "")
+						 (git-gutter-mode "")
+						 (highlight-symbol-mode "")
+						 (eldoc-mode "")
+						 (magit-auto-revert-mode "")))
 
 (--each diminish-modes
   (let ((mode-name (car it))
         (lighter (cadr it)))
-	(eval-after-load mode-name
+	(eval-after-load (symbol-name mode-name)
       (diminish mode-name lighter))))
