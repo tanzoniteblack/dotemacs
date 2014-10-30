bb25e239c39a68fdde04a79fb91d24af4fcbafb1
Ryan Smith <rnsmith2@gmail.com>
2014-10-30 15:40:57 -0400

    Make js2-mode default .js files
    Also re-write custom keyboard shortcuts for tern-mode with normal `kbd` macro

--- config/js-config.el
+++ config/js-config.el
@@ -1,17 +1,18 @@
 (require 'js2-mode)
-(autoload 'tern-mode "tern.el" nil t)
+(require 'tern)
 (add-hook 'js2-mode-hook 'tern-mode)
 
-(eval-after-load 'tern
-  '(progn
-     (require 'company-tern)
-     (add-to-list 'company-backends 'company-tern)
-     (define-key tern-mode-keymap [(meta ?.)] 'tern-find-definition)
-     (define-key tern-mode-keymap [(control meta ?.)] 'tern-find-definition-by-name)
-     (define-key tern-mode-keymap [(meta ?,)] 'tern-pop-find-definition)
-     (define-key tern-mode-keymap [(control ?c) (control ?r)] 'tern-rename-variable)
-     (define-key tern-mode-keymap [(control ?c) (control ?c)] 'tern-get-type)
-     (define-key tern-mode-keymap [(control ?c) (control ?d)] 'tern-get-docs)))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+(require 'company-tern)
+(add-to-list 'company-backends 'company-tern)
+(define-key tern-mode-keymap (kbd "M-.") 'tern-find-definition)
+(define-key tern-mode-keymap (kbd "C-M-.") 'tern-find-definition-by-name)
+(define-key tern-mode-keymap (kbd "M-,") 'tern-pop-find-definition)
+(define-key tern-mode-keymap (kbd "C-c C-r") 'tern-rename-variable)
+(define-key tern-mode-keymap (kbd "C-c C-c") 'tern-get-type)
+(define-key tern-mode-keymap (kbd "C-c C-d") 'tern-get-docs)
+(define-key tern-mode-keymap (kbd "M-<return>") 'tern-get-docs)
 
 (add-to-list 'auto-mode-alist '(".tern-project" . json-mode))
 (add-to-list 'auto-mode-alist '(".jshintrc" . json-mode))
