2f22068078e968e6534cbe39068664be61367f8b
Ryan Smith <rnsmith2@gmail.com>
2014-11-05 11:07:32 -0800

    Turn on auto-fill-mode in org-mode automatically

--- config/org-config.el
+++ config/org-config.el
@@ -45,3 +45,4 @@
 (add-hook 'org-shiftleft-final-hook 'windmove-left)
 (add-hook 'org-shiftdown-final-hook 'windmove-down)
 (add-hook 'org-shiftright-final-hook 'windmove-right)
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
