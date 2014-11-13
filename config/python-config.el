7b0b223166a4712d968c12ca11eb7df8c0b7ff85
Ryan Smith <rnsmith2@gmail.com>
2014-11-13 08:12:51 -0800

    Set elpy backend pre-enabling

--- config/python-config.el
+++ config/python-config.el
@@ -1,8 +1,8 @@
+(setq elpy-rpc-backend "jedi")
 (elpy-enable)
 
 (define-key elpy-mode-map (kbd "M-.") 'elpy-goto-definition)
 (define-key elpy-mode-map (kbd "M-,") 'pop-tag-mark)
-(setq elpy-rpc-backend "jedi")
 
 (add-hook 'python-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'python-mode-hook 'highlight-symbol-mode)
