6331c36b89627c5ce17aa472da700b08d5eb9c6f
Ryan Smith <rnsmith2@gmail.com>
2014-11-20 22:15:30 -0800

    More sharp quotes

--- config/java-config.el
+++ config/java-config.el
@@ -3,11 +3,11 @@
 (load "semantic/loaddefs.el")
 (semantic-mode 1)
 (require 'malabar-mode)
-(add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+(add-to-list #'auto-mode-alist '("\\.java\\'" . malabar-mode))
 (require 'dtrt-indent)
-(add-hook 'java-mode-hook #'dtrt-indent-mode)
+(add-hook #'java-mode-hook #'dtrt-indent-mode)
 
-(add-to-list 'company-semantic-modes 'malabar-mode)
+(add-to-list #'company-semantic-modes #'malabar-mode)
 
 (define-key malabar-mode-map (kbd "M-.") #'malabar-jump-to-thing)
 (define-key malabar-mode-map (kbd "M-,") #'pop-global-mark)
