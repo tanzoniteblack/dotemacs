6c325c69921222caebbfca9ce88a7d5b5930d754
Ryan Smith <ryan@ip-192-168-0-90.ec2.internal>
2015-01-05 14:10:28 -0800

    More M-<RET> doc keybindings
    Add get doc in elpy with M-<RET>

--- config/python-config.el
+++ config/python-config.el
@@ -3,6 +3,7 @@
 
 (define-key elpy-mode-map (kbd "M-.") #'elpy-goto-definition)
 (define-key elpy-mode-map (kbd "M-,") #'pop-tag-mark)
+(define-key elpy-mode-map (kbd "M-<RET>") #'elpy-doc)
 
 (add-hook 'python-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'python-mode-hook #'highlight-symbol-mode)
