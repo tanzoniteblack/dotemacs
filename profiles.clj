a09d270699290f1173ed55b7be19265c7f7a7199
Ryan Smith <ryan@ip-192-168-0-90.ec2.internal>
2015-03-03 11:07:26 -0800

    Update profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,7 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-ancient "0.5.5"]]
-        :dependencies [[acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]}}
+                  [refactor-nrepl "0.2.2"]]
+        :dependencies [[acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]
+                       [pjstadig/humane-test-output "0.6.0"]
+                       [org.clojure/tools.nrepl "0.2.7"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}}
