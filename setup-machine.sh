319774ae5ead6f3ec29b07fc5d1b911d48798040
Ryan Smith <ryan@ip-192-168-0-90.ec2.internal>
2015-03-03 13:53:17 -0800

    Checkin tmux.conf

--- setup-machine.sh
+++ setup-machine.sh
@@ -1,16 +1,30 @@
+# Setup git
 git config --global user.email "ryan@yummly.com"
 git config --global user.name "Ryan Smith"
 git config --global color.ui auto
+
+# Setup emacs
 rm ~/.emacs.d.bak -rf
 mv -f ~/.emacs.d ~/.emacs.d.bak
 git clone https://github.com/tanzoniteblack/dotemacs.git ~/.emacs.d
+
+# Create ~/.lein/profiles.clj
 mkdir -p ~/.lein/
 sudo chown -R $USER ~/.lein/
-cp ~/.emacs.d/profiles.clj ~/.lein/profiles.clj
+ln ~/.emacs.d/profiles.clj ~/.lein/profiles.clj
+
+# Install helper programs (Ubuntu only)
+sudo apt-get install -y zsh aspell htop nethogs
+
+# Setup zsh
 rm -rf ~/.oh-my-zsh.bak
 mv ~/.oh-my-zsh ~/.oh-my-zsh.bak
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh/
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/
-cp ~/.emacs.d/.zshrc ~/.zshrc
-sudo apt-get install -y zsh aspell htop nethogs
+rm ~/.zshrc
+ln ~/.emacs.d/.zshrc ~/.zshrc
 sudo chsh $USER -s /bin/zsh
+
+# Setup tmux
+rm ~/.tmux.conf
+ln ~/.emacs.d/tmux.conf ~/.tmux.conf
