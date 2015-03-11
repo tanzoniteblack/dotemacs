819140196a077725d8d0f28257f0a1096697cb93
Ryan Smith <rnsmith2@gmail.com>
2015-03-11 07:43:40 -0700

    Add aspell path

--- config/windows-config.el
+++ config/windows-config.el
@@ -1,3 +1,4 @@
 ;; Windows-NT specific configuration
 (setenv "PATH" (concat "C:\\Program Files (x86)\\Git\\bin;" (getenv "PATH")))
 (add-to-list 'exec-path "c:/Program Files (x86)/Git/bin")
+(add-to-list 'exec-path "c:/Program Files (x86)/Aspell/bin")
