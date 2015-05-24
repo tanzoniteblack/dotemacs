2856b048a2b0bf5577b21a6db3cc02b7e6129794
Ryan Smith <rnsmith2@gmail.com>
2015-05-24 08:53:16 -0700

    Add python for windows

--- config/windows-config.el
+++ config/windows-config.el
@@ -1,6 +1,8 @@
 ;; Windows-NT specific configuration
 (setenv "PATH" (concat "C:\\Program Files (x86)\\Git\\bin;"
 					   "C:\\Program Files\\Java\\jdk1.8.0_40\\bin;"
+					   "C:\\Python27;"
+					   "C:\\Python27\\Scripts;"
 					   (getenv "PATH")))
 (add-to-list 'exec-path "c:/Program Files (x86)/Git/bin")
 (add-to-list 'exec-path "c:/Program Files (x86)/Aspell/bin")
