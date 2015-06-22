9761fd10dc4e71bcf59010838e1a60a02eef9acd
Ryan Smith <rnsmith2@gmail.com>
2015-06-22 16:58:56 -0700

    Update lein profiles
    - Use latest cider-nrepl
    - Require lein-ancient
    - Require criterium
    - Remove clj-refactor nrepl, it was annoying

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,7 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [refactor-nrepl "1.0.0"]]
-        :dependencies [[acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]
-                       [pjstadig/humane-test-output "0.6.0"]
-                       [org.clojure/tools.nrepl "0.2.7"]]
+{:user {:plugins [[cider/cider-nrepl "0.9.1-SNAPSHOT"]
+                  [lein-ancient "0.6.7"]]
+        :dependencies [[pjstadig/humane-test-output "0.6.0"]
+                       [org.clojure/tools.nrepl "0.2.7"]
+                       [criterium "0.4.3"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
