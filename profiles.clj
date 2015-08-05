fd27f4773ba60b839cee21078d9aaad435c5e8d2
Ryan Smith <rnsmith2@gmail.com>
2015-08-05 11:53:47 -0700

    Use refactor-nrepl

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [lein-ancient "0.6.7"]]
+                  [lein-ancient "0.6.7"]
+                  [refactor-nrepl "1.1.0"]]
         :dependencies [[pjstadig/humane-test-output "0.6.0"]
                        [org.clojure/tools.nrepl "0.2.7"]
                        [criterium "0.4.3"]]
