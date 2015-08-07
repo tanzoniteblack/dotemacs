187b73d88d732b8c92e623ee007f5c608231abca
Ryan Smith <rnsmith2@gmail.com>
2015-08-07 10:51:20 -0700

    Use smartparens in web-mode

--- config/settings.el
+++ config/settings.el
@@ -639,8 +639,19 @@ the checking happens for all pairs in auto-minor-mode-alist"
                (add-to-list 'auto-mode-alist '("\\.mustache$" . web-mode))
                (add-to-list 'auto-mode-alist '("\\.djhtml$" . web-mode))
                (add-to-list 'auto-mode-alist '("\\.html$" . web-mode)))
-  :config ;; Autocomplete end tag when finished writing opening tag
-  (setq web-mode-auto-close-style 2))
+  :config (progn (defun my-web-mode-hook ()
+                   (setq web-mode-enable-auto-pairing nil))
+
+                 (add-hook 'web-mode-hook  'my-web-mode-hook)
+
+                 (defun sp-web-mode-is-code-context (id action context)
+                   (when (and (eq action 'insert)
+                              (not (or (get-text-property (point) 'part-side)
+                                       (get-text-property (point) 'block-side))))
+
+                     t))
+
+                 (sp-local-pair 'web-mode "<" nil :when '(sp-web-mode-is-code-context))))
 
 (use-package ruby-mode
   :ensure t
