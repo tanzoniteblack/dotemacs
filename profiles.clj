31f132aac9c35c55b7b88aa36c9884732f8d7012
Ryan Smith <rnsmith2@gmail.com>
2015-08-17 17:52:56 -0700

    Remove squiggly clojure lein dep

--- profiles.clj
+++ profiles.clj
@@ -3,7 +3,6 @@
                   [refactor-nrepl "1.1.0"]]
         :dependencies [[pjstadig/humane-test-output "0.6.0"]
                        [org.clojure/tools.nrepl "0.2.7"]
-                       [acyclic/squiggly-clojure "0.1.3-SNAPSHOT"]
                        [criterium "0.4.3"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
