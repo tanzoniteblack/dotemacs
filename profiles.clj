c349ca04fc00255b1a0e8a648fe4a9ef71461f37
Ryan Smith <rnsmith2@gmail.com>
2015-10-30 11:18:41 -0700

    Update lein profile deps

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,8 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [lein-ancient "0.6.7"]
-                  [refactor-nrepl "1.2.0-SNAPSHOT"]]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
         :dependencies [[pjstadig/humane-test-output "0.6.0"]
-                       [org.clojure/tools.nrepl "0.2.7"]
+                       [org.clojure/tools.nrepl "0.2.11"]
                        [criterium "0.4.3"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
