2612d07e26fd1b0c68c4e664f61e8d2b8d37e262
Ryan Smith <rnsmith2@gmail.com>
2016-02-08 14:41:38 -0800

    Rely on alpha refactor-nrepl
    Makes it stop downloading snapshots every day...

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
                   [lein-ancient "0.6.7"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
+                  [refactor-nrepl "2.1.0-alpha1"]]
         :dependencies [[pjstadig/humane-test-output "0.6.0"]
                        [org.clojure/tools.nrepl "0.2.12"]
                        [criterium "0.4.3"]]
