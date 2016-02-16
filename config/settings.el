bde5f2d8dc6a252f46ad070324ea6522d20727a8
Ryan Smith <rnsmith2@gmail.com>
2016-02-16 11:03:35 -0800

    Take advantage of new cider-jack-in functionality
    Don't need to have cider and clj-refactor dependencies in my
    `~/.lein/profiles.clj` anymore, because they're now injected when I do a
    `cider-jack-in` call. Should cause lein command line options to be
    faster now.

--- config/settings.el
+++ config/settings.el
@@ -460,6 +460,8 @@ the checking happens for all pairs in auto-minor-mode-alist"
                                   (define-key cider-mode-map (kbd "M-RET") 'cider-doc)
 								  (define-key cider-mode-map (kbd "C-c SPC") 'avy-goto-word-1)
 
+								  (add-to-list 'cider-jack-in-dependencies ("criterium" "0.4.3"))
+
 								  (use-package clj-refactor
                                     :ensure t
                                     :config (progn (setq cljr-suppress-middleware-warnings t)
--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [lein-ancient "0.6.7"]
-                  [refactor-nrepl "2.1.0-alpha1"]]
-        :dependencies [[pjstadig/humane-test-output "0.6.0"]
-                       [org.clojure/tools.nrepl "0.2.12"]
-                       [criterium "0.4.3"]]
+{:user {:dependencies [[pjstadig/humane-test-output "0.6.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
