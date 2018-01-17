ddf6426358bc67beddf3d97fdcc245e090703753
Ryan Smith <rnsmith2@gmail.com>
2018-01-17 10:00:19 -0800

    Tabs are evil

--- init.el
+++ init.el
@@ -951,7 +951,8 @@ magit-mode."
 (use-package cc-mode
   :defer t
   :config (progn (setq-default c-basic-offset 4 c-default-style "linux")
-                 (setq-default tab-width 4 indent-tabs-mode t)
+                 (setq-default tab-width 4
+							   indent-tabs-mode nil)
                  (add-hook 'java-mode-hook 'subword-mode)))
 
 (use-package dtrt-indent
